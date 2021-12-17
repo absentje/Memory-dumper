45ed53e8c8560c3b3ba37c0ac8ca63869a35fb42
r.khanov <r.khanov@cognitivepilot.com>
2021-12-17 18:15:16 +0300

    fixed result output

--- src/memory_dumper.cpp
+++ src/memory_dumper.cpp
@@ -23,13 +23,24 @@ public:
   MsvcMemoryDumper( const std::string& dumper_name )
     : MemoryDumper( dumper_name )
   {
+    _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
+    _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
+    _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
+    _CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDOUT);
+    _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
+    _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDOUT);
+
     _CrtMemCheckpoint(&s1);
   }
   virtual ~MsvcMemoryDumper() override
   {
+    std::cout << "========================================================\n";
+    std::cout << "MemoryDumper: " << dumper_name_ << std::endl;
+    std::cout << "========================================================\n";
     _CrtMemCheckpoint(&s2);
     _CrtMemDifference( &s3, &s1, &s2 );
     _CrtMemDumpStatistics( &s3 );
+    std::cout << "========================================================\n\n";
   }
 private:
   _CrtMemState s1 = {}, s2 = {}, s3 = {};
