fcc40fd52f266a93dbbe19a14827647c30631e9a
r.khanov <r.khanov@cognitivepilot.com>
2021-12-17 18:58:45 +0300

    fix[1000-7]

--- src/memory_dumper.cpp
+++ src/memory_dumper.cpp
@@ -7,7 +7,6 @@
 #include <stdio.h>
 #endif
 
-
 namespace MemoryLeaksDetection
 {
 MemoryDumper::MemoryDumper( const std::string& dumper_name )
@@ -36,7 +35,25 @@ public:
   {
     _CrtMemCheckpoint(&s2);
     _CrtMemDifference( &s3, &s1, &s2 );
-    if ( bool have_diff = (s3.lTotalCount > 0) )
+
+  #define _FREE_BLOCK      0
+  #define _NORMAL_BLOCK    1
+  #define _CRT_BLOCK       2
+  #define _IGNORE_BLOCK    3
+  #define _CLIENT_BLOCK    4
+
+    auto is_not_empty = [this]( int block_type )
+    {
+      return (int)s3.lCounts[block_type] > 0 && (int)s3.lSizes[block_type] > 0;
+    };
+
+    bool have_diff = is_not_empty(_FREE_BLOCK) ||
+      is_not_empty(_NORMAL_BLOCK) ||
+      is_not_empty(_CRT_BLOCK) ||
+      is_not_empty(_IGNORE_BLOCK) ||
+      is_not_empty( _CLIENT_BLOCK );
+
+    if ( have_diff )
     {
       std::cout << "========================================================\n";
       std::cout << "MemoryDumper: " << dumper_name_ << std::endl;
